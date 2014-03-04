6f6c96a9a39dbad316c475855cbd69d45807e6d7
niekas <albertas.gimbutas@mii.vu.lt>
2014-03-04 12:49:50 +0200

    Add EmailForm and PasswordRecoveryForm.

--- src/hack4lt/forms.py
+++ src/hack4lt/forms.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate
 from django.utils.translation import ugettext_lazy as _
 from django.forms.util import ErrorList
 
+from django.utils.http import base36_to_int
+from django.contrib.auth.tokens import default_token_generator
+
 from hack4lt.models import (
     Hacker,
     Task1,
@@ -62,6 +65,45 @@ class LoginForm(forms.Form):
             raise forms.ValidationError(_('Username or password is incorrect'))
         cleaned_data['user'] = user
         return cleaned_data
+
+
+class EmailForm(forms.Form):
+    email = forms.EmailField(label=_('E-mail'), max_length=100)
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not Hacker.objects.filter(email=email, is_active=True).exists():
+            raise forms.ValidationError(_('Please enter correct email address'))
+        return email
+
+
+class PasswordRecoveryForm(forms.Form):
+    password = forms.CharField(label=_('Password'), max_length=128,
+                        widget=forms.PasswordInput(render_value=False))
+    password_repeat = forms.CharField(label=_('Repeat Password'),
+            max_length=128, widget=forms.PasswordInput(render_value=False))
+
+    def is_valid(self):
+        valid = super(PasswordRecoveryForm, self).is_valid()
+        if not valid:
+            return valid
+
+        first_password = self.cleaned_data.get('password')
+        repeat_password = self.cleaned_data.get('password_repeat')
+
+        if first_password == repeat_password:
+            return True
+        errors = self._errors.setdefault('password_repeat', ErrorList())
+        errors.append(u'Passwords do not match')
+        return False
+
+    def save(self, uidb36, token):
+        password = self.cleaned_data.get('password')
+        user = Hacker.objects.get(pk=base36_to_int(uidb36))
+        if default_token_generator.check_token(user, token):
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class Task1Form(forms.ModelForm):
